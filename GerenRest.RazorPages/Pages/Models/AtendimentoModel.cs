1b5b4939839f404904f6aa387ee8f11c74ff36b3
dbonfleur <danibonfleur@hotmail.com>
2023-04-10 09:09:55 -0300

    versao2.1

--- GerenRest.RazorPages/Pages/Models/AtendimentoModel.cs
+++ GerenRest.RazorPages/Pages/Models/AtendimentoModel.cs
@@ -2,6 +2,7 @@ namespace GerenRest.RazorPages.Models {
     public class AtendimentoModel {
         public int? AtendimentoID { get; set; }
         public MesaModel? MesaAtendida { get; set; }
-        
+        public GarconModel? GarconResponsavel { get; set; }
+        public List<ProdutoModel>? ListaProdutos { get; set; }
     }
 }
